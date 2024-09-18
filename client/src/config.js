40fa3666c000fac9904aa29559fd24f265092ac2
David Sebold <d.seibold@icloud.com>
2024-09-17 22:46:59 -0700

    About text correction

--- client/src/config.js
+++ client/src/config.js
@@ -10,7 +10,7 @@ export const COPYRIGHT_INFO =
 export const CONTACT_EMAIL = "d.seibold@icloud.com";
 export const CONTACT_NAME = "David Seibold";
 export const CONTACT_BIO =
-  "Hi, I'm David! I am an entry-level full-stack software engineer with over [0] years of professional experience, specializing in [specialty x, y and z]. My expertise lies in [architectures, applications, and languages q, r, s]. ";
+  "From M&A spreadsheets to full-stack sprints, I’ve traded in my calculator for code! As an entry-level full-stack software engineer, I bring a unique blend of strategic thinking and technical execution to every project. While I’m just getting started in the coding world, my passion for building scalable, efficient applications is growing every day. With hands-on experience in JavaScript, React.js, Node.js, and MongoDB, I’m diving deep into the world of software development, learning new skills and applying them to real-world challenges. Whether it's crafting smooth user interfaces or designing efficient back-end systems, I love creating solutions that make an impact.";
 
 export const CONTACT_PHONE = "+1(818)274-9823";
 
