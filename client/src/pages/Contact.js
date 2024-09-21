93426ad08dc02bda5f5165c664defb9ac8898647
David Sebold <d.seibold@icloud.com>
2024-09-20 18:34:41 -0700

    contact page color adjustment

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -57,7 +57,7 @@ function ContactForm() {
           <div>
             <label
               htmlFor="first-name"
-              className="block text-sm font-semibold leading-6 text-orange-700"
+              className="block text-sm font-semibold leading-6 text-lime-700"
             >
               First name
             </label>
@@ -67,7 +67,7 @@ function ContactForm() {
                 name="first-name"
                 id="first-name"
                 autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-orange-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-lime-900 shadow-sm ring-1 ring-inset ring-lime-300 placeholder:text-lime-400 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
               />
@@ -77,7 +77,7 @@ function ContactForm() {
           <div>
             <label
               htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-orange-700"
+              className="block text-sm font-semibold leading-6 text-lime-700"
             >
               Last name
             </label>
@@ -87,7 +87,7 @@ function ContactForm() {
                 name="last-name"
                 id="last-name"
                 autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-orange-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-lime-900 shadow-sm ring-1 ring-inset ring-lime-300 placeholder:text-lime-400 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
@@ -97,7 +97,7 @@ function ContactForm() {
           <div className="sm:col-span-2">
             <label
               htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-orange-700"
+              className="block text-sm font-semibold leading-6 text-lime-700"
             >
               Email
             </label>
@@ -107,7 +107,7 @@ function ContactForm() {
                 name="email"
                 id="email"
                 autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-orange-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-lime-900 shadow-sm ring-1 ring-inset ring-lime-300 placeholder:text-lime-400 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -117,7 +117,7 @@ function ContactForm() {
           <div className="sm:col-span-2">
             <label
               htmlFor="comments"
-              className="block text-sm font-semibold leading-6 text-orange-700"
+              className="block text-sm font-semibold leading-6 text-lime-700"
             >
               Comments
             </label>
@@ -125,7 +125,7 @@ function ContactForm() {
               <textarea
                 name="comments"
                 id="comments"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-orange-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-orange-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-lime-900 shadow-sm ring-1 ring-inset ring-lime-300 placeholder:text-lime-400 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
                 value={comments}
                 placeholder="Leave a comment"
                 onChange={(e) => setComments(e.target.value)}
@@ -136,7 +136,7 @@ function ContactForm() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-gradient-to-r from-[#EFBD19] to-[#8000FF] px-3.5 py-2.5 text-center text-sm font-semibold text-[#f3eacc] shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+            className="block w-full rounded-md bg-gradient-to-r from-[#EFBD19] to-[#8000FF] px-3.5 py-2.5 text-center text-sm font-semibold text-[#f3eacc] shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
           >
             Leave me a message
           </button>
