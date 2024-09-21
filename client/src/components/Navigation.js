f1b404011ed44c4d13650b5d276b60c94c0ca611
David Sebold <d.seibold@icloud.com>
2024-09-21 02:15:16 -0700

    Nav curve experiment revert

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -25,74 +25,102 @@ const Navigation = () => {
 
   return (
     <>
-      {/* Arched Navigation Bar */}
-      <nav className="relative bg-[#282828] p-6 sm:p-8 flex justify-center items-center h-96">
-        {/* Arched Layout */}
-        <div className="flex justify-center items-center w-full h-full relative">
-          {/* Left - Logo and Title */}
-          <div className="absolute left-1/4 top-1/3 transform -rotate-10">
-            <span className="text-white font-bold text-xl sm:text-2xl">
-              &#123; David Seibold &#125;
-            </span>
-          </div>
+      <nav className="bg-[#282828] p-6 sm:p-8 flex flex-col sm:flex-row items-center sm:justify-between">
+        {/* Left Section - Logo and Title */}
+        <div className="flex items-center mb-4 sm:mb-0">
+          <span className="text-[#F3EACC] font-bold text-2xl sm:text-3xl mr-4">
+            &#123; David Seibold &#125;
+          </span>
+          <img
+            src={logo}
+            alt="David Seibold's personal logo"
+            className="w-10 h-10 sm:w-12 sm:h-12 rounded-full"
+          />
+        </div>
 
-          {/* Navigation Links - Curved Around */}
-          <ul className="absolute top-1/4 flex space-x-4 sm:space-x-8 transform">
-            <li className="transform rotate-10">
+        {/* Center Section - Navigation Links */}
+        <ul className="flex flex-col sm:flex-row sm:justify-center sm:space-x-6 text-center sm:text-center">
+          <li>
+            <Link
+              to="/"
+              className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blogs"
+              className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+            >
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+            >
+              Contact
+            </Link>
+          </li>
+          {!auth.isAuthenticated ? (
+            <li>
               <Link
-                to="/"
-                className="text-white hover:text-gray-400 text-xl sm:text-2xl"
+                to="/login"
+                className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
               >
-                Home
+                Login
               </Link>
             </li>
-            <li className="transform rotate-5">
-              <Link
-                to="/blogs"
-                className="text-white hover:text-gray-400 text-xl sm:text-2xl"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li className="transform rotate-0">
-              <Link
-                to="/projects"
-                className="text-white hover:text-gray-400 text-xl sm:text-2xl"
-              >
-                Projects
-              </Link>
-            </li>
-            <li className="transform -rotate-5">
-              <Link
-                to="/about"
-                className="text-white hover:text-gray-400 text-xl sm:text-2xl"
-              >
-                About
-              </Link>
-            </li>
-            <li className="transform -rotate-10">
-              <Link
-                to="/contact"
-                className="text-white hover:text-gray-400 text-xl sm:text-2xl"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+          ) : (
+            <>
+              <li>
+                <Link
+                  to="/admin-dashboard"
+                  className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+                >
+                  Admin Dashboard
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="text-[#F3EACC] hover:text-gray-400 text-xl sm:text-2xl"
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
 
-          {/* Right - City and Clock */}
-          <div className="absolute right-1/4 top-1/3 transform rotate-10 text-right">
-            <p className="font-bold text-2xl sm:text-3xl text-white">
-              San Diego, CA
-            </p>
-            <p className="text-xl sm:text-3xl text-lime-400 font-mono transition-all duration-500 ease-in-out">
-              <i className="fa fa-clock-o mr-2"></i> {currentTime}
-            </p>
-          </div>
+        {/* Right Section - City and Clock */}
+        <div className="text-[#F3EACC] text-right mt-4 sm:mt-0">
+          <p className="font-bold text-2xl sm:text-3xl">San Diego, CA</p>
+          <p className="text-xl sm:text-3xl text-lime-400 font-mono transition-all duration-500 ease-in-out">
+            <i className="fa fa-clock-o mr-2"></i> {currentTime}
+          </p>
         </div>
       </nav>
 
-      {/* Centered large logo below navigation */}
+      {/* Centered medium-sized logo below the navigation bar */}
       <div className="flex justify-center p-4 bg-[#282828]">
         <img
           src={logo}
